c71e7c20a80c83c53fb9d12eee4c53c5fae1575c
Xu Ma <xuma@my.unt.edu>
2021-12-13 03:31:22 -0500

    Update pointtransformer_seg_pos.py

--- model/pointtransformer/pointtransformer_seg_pos.py
+++ model/pointtransformer/pointtransformer_seg_pos.py
@@ -11,9 +11,9 @@ class PointTransformerLayer(nn.Module):
         self.out_planes = out_planes
         self.share_planes = share_planes
         self.nsample = nsample
-        self.linear_q = nn.Linear(in_planes, mid_planes)
-        self.linear_k = nn.Linear(in_planes, mid_planes)
-        self.linear_v = nn.Linear(in_planes, out_planes)
+        # self.linear_q = nn.Linear(in_planes, mid_planes)
+        # self.linear_k = nn.Linear(in_planes, mid_planes)
+        # self.linear_v = nn.Linear(in_planes, out_planes)
         # self.linear_p = nn.Sequential(nn.Linear(3, 3), nn.BatchNorm1d(3), nn.ReLU(inplace=True), nn.Linear(3, out_planes))
         self.linear_w = nn.Sequential(nn.BatchNorm1d(mid_planes), nn.ReLU(inplace=True),
                                     nn.Linear(mid_planes, mid_planes // share_planes),
@@ -27,7 +27,7 @@ class PointTransformerLayer(nn.Module):
         x_k = pointops.queryandgroup(self.nsample, p, p, x, None, o, o, use_xyz=True)  # (n, nsample, 3+c)
         # x_v = pointops.queryandgroup(self.nsample, p, p, x_v, None, o, o, use_xyz=False)  # (n, nsample, c)
         p_r, x_k = x_k[:, :, 0:3], x_k[:, :, 3:]
-        # for i, layer in enumerate(self.linear_p): p_r = layer(p_r.transpose(1, 2).contiguous()).transpose(1, 2).contiguous() if i == 1 else layer(p_r)    # (n, nsample, c)
+        for i, layer in enumerate(self.linear_p): p_r = layer(p_r.transpose(1, 2).contiguous()).transpose(1, 2).contiguous() if i == 1 else layer(p_r)    # (n, nsample, c)
         # w = x_k - x_q.unsqueeze(1) + p_r.view(p_r.shape[0], p_r.shape[1], self.out_planes // self.mid_planes, self.mid_planes).sum(2)  # (n, nsample, c)
         w = p_r.view(p_r.shape[0], p_r.shape[1], self.out_planes // self.mid_planes, self.mid_planes).sum(2)  # (n, nsample, c)
         for i, layer in enumerate(self.linear_w): w = layer(w.transpose(1, 2).contiguous()).transpose(1, 2).contiguous() if i % 3 == 0 else layer(w)
